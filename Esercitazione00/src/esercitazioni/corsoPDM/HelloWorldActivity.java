6ad628ad5a73ac6382da711b2e6b4b8eef0c5f1b
Emiliano Del Signore <emilianodelsignore@gmail.com>
2012-02-01 15:18:50 +0100

    Prima esercitazione modificata

--- Esercitazione00/src/esercitazioni/corsoPDM/HelloWorldActivity.java
+++ Esercitazione00/src/esercitazioni/corsoPDM/HelloWorldActivity.java
@@ -3,10 +3,12 @@ package esercitazioni.corsoPDM;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HelloWorldActivity extends Activity {
+public class HelloWorldActivity extends Activity 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
